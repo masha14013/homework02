d4e5ce1edf75fd1d2141668c75e037f3c2f813dc
masha14013 <masha14013@rambler.ru>
2022-10-23 14:01:40 +0300

    app structure changed

--- src/middlewares/input-validation-middleware.ts
+++ src/middlewares/input-validation-middleware.ts
@@ -4,7 +4,7 @@ import {validationResult} from "express-validator";
 export const inputValidationMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        const error = errors.array({onlyFirstError: false}).map(el => {
+        const error = errors.array({onlyFirstError: true}).map(el => {
             return {
                 message: el.msg,
                 field: el.param
