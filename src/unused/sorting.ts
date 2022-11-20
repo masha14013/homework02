977e3b64f8c301c5ec309c7d6e793c0f6157f112
masha14013 <masha14013@rambler.ru>
2022-11-20 17:02:11 +0300

    app structure changed - 5th homework

---
+++ src/unused/sorting.ts
@@ -0,0 +1,25 @@
+// let users = [
+//     {id: 'dsdf2-sdfs-23', name: 'dimych', age: 34},
+//     {id: 'csdc3-ddfs-11', name: 'ivan', age: 30},
+//     {id: 'dsdc1-dafs-31', name: 'ignat', age: 20},
+//     {id: '6sac3-1d1s-21', name: 'artem', age: 20},
+//     {id: 'qsdf3-sdfs-25', name: 'kolya', age: 25},
+//     {id: 'qsdf3-sfrs-21', name: 'kolya', age: 32},
+//     {id: 'qsdf3-stts-99', name: 'kolya', age: 92}
+// ]
+//
+// const getUsers = () => {
+//     return [...users].sort((u1, u2) => {
+//         if (u1.name < u2.name) return -1
+//         if (u1.name > u2.name) return 1
+//
+//         if (u1.age < u2.age) return 1
+//         if (u1.age > u2.age) return -1
+//         return 0
+//     })
+// }
+//
+// console.log(getUsers())
+//
+//
+
