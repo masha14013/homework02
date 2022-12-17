4976730ce7d91a9bdcc0a492b85b2cff3cf901b7
masha14013 <masha14013@rambler.ru>
2022-12-17 16:55:58 +0300

    changed 7th homework

--- src/managers/email-manager.ts
+++ src/managers/email-manager.ts
@@ -3,6 +3,9 @@ import {UsersType} from "../repositories/db";
 
 export const emailManager = {
     async sendPasswordRecoveryMessage(user: UsersType) {
-        await emailAdapter.sendEmail(user.accountData.email, "Confirm email", "<div><a>Confirm email, click here</a></div>")
+        await emailAdapter.sendEmail(
+                user.accountData.email,
+                "Confirm email",
+                `<div><a href="https://some-front.com/confirm-registration?code=${user.emailConfirmation.confirmationCode}">Confirm email, click here</a></div>`)
     }
 }
